efa514f6c427c9d60ab61f58b07acd85a18fc4d2
fscarponi <fscarponi@gmail.com>
2021-03-14 14:50:27 +0100

    WIP - Echo bot is working!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import com.github.lamba92.kotlingram.api.generated.InlineQueryResultPhoto
+import com.github.lamba92.kotlingram.api.generated.getMe
 import com.github.lamba92.kotlingram.builder.buildPollingBot
 import com.github.lamba92.kotlingram.builder.respond
 import com.github.lamba92.kotlingram.builder.respondPhoto
