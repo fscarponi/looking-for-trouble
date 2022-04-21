9551fa7779dc12b42b01767af7dd3d65a4fdbe7f
fscarponi <fscarponi@gmail.com>
2022-04-21 22:02:39 +0200

    now this bot do something!!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,12 @@
 import dev.inmo.tgbotapi.bot.Ktor.telegramBot
 import dev.inmo.tgbotapi.extensions.api.bot.getMe
-import dev.inmo.tgbotapi.extensions.api.chat.get.getChat
+import dev.inmo.tgbotapi.extensions.api.send.media.sendPhoto
 import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.api.send.sendMessage
-import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviour
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
-import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.retrieveAccumulatedUpdates
-import dev.inmo.tgbotapi.updateshandlers.FlowsUpdatesFilter
-import kotlinx.coroutines.coroutineScope
+import dev.inmo.tgbotapi.requests.abstracts.toInputFile
+import java.io.File
 
 
 val customMessage = buildString {
@@ -19,16 +17,20 @@ val customMessage = buildString {
 val media = "https://www.tc-web.it/wp-content/uploads/2019/01/java.jpg"
 
 @ExperimentalStdlibApi
-suspend fun main(): Unit{
+suspend fun main(): Unit {
     println("Avvio bot looking for trouble")
-    val bot = telegramBot( System.getenv("LFT_BOT_API_TOKEN"))
+    val bot = telegramBot(System.getenv("LFT_BOT_API_TOKEN"))
+    val url = object {}.javaClass.getResource("placeHolderMEME.jpg")?.file
+    val inputFile = File(url)
+
 
     bot.buildBehaviourWithLongPolling {
         println(getMe())
 
         onCommand("start") {
             reply(it, "Hi:)")
-            sendMessage(it.chat,"random message!!")
+            sendMessage(it.chat, "random message!!")
+            sendPhoto(it.chat, inputFile.toInputFile())
         }
     }.join()
     println("Bot Terminato!")
